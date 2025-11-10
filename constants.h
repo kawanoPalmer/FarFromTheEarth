b65c8d263b35bfbb4b9356cd800e66bc04aa6979
kiiroao <kumasota1215@icloud.com>
2025-11-10 16:47:04 +0900

    Add files via upload

---
+++ constants.h
@@ -0,0 +1,24 @@
+#ifndef _CONSTANTS_H_
+#define _CONSTANTS_H_
+
+#include <netinet/in.h>
+
+#define MAX_NUM_CLIENTS 4
+#define MAX_LEN_NAME 32
+#define MAX_LEN_ADDR 32
+#define PORT 8888
+#define BROADCAST -1
+
+typedef struct {
+    int cid;                          // クライアントID（0〜3）
+    int sock;                         // ソケット番号
+    struct sockaddr_in addr;          // アドレス情報
+    char name[MAX_LEN_NAME];          // 名前
+} CLIENT;
+
+typedef struct {
+    int cid;    // 送信者ID
+    char key;   // 押したキー
+} KEY_EVENT;
+
+#endif
